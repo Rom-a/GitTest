cedb78fcad22848a3890df1d199fb5077af60cd4
Roman <Maslov.Roman.ua@gmail.com>
2020-06-02 15:05:57 +0300

    Version 5

--- GitTestPro/GitTestPro/Program.cs
+++ GitTestPro/GitTestPro/Program.cs
@@ -6,7 +6,7 @@ namespace GitTestPro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Version 2.0");
+            Console.WriteLine("Version 5.0");
         }
 
         static void TestMethodA()
