278b624c6a6894f493bd5ea45126beb3de4836cc
Roman <Maslov.Roman.ua@gmail.com>
2020-06-02 14:18:59 +0300

    Version 1

---
+++ GitTestPro/GitTestPro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTestPro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Version 1.0");
+        }
+    }
+}
